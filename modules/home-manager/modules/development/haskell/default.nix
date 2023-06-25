f9ea8a55d2cad276a25c4bc19be96002149aadde
Pangz Feng <43704063+fplaotouer@users.noreply.github.com>
2023-06-25 13:11:24 +0800

    fix: make sure dir `"$XDG_STATE_HOME"/bash` exists

--- modules/home-manager/modules/development/haskell/default.nix
+++ modules/home-manager/modules/development/haskell/default.nix
@@ -10,7 +10,7 @@
 
   home.sessionVariables = {
     STACK_ROOT = "${config.xdg.dataHome}/stack";
-    GHCUP_USE_XDG_DIRS = "WHY NOT";
+    GHCUP_USE_XDG_DIRS = "WHY_NOT";
   };
 
   # when `GHCUP_USE_XDG_DIRS` is set, GHCup installs binaries to `XDG_BIN_HOME` (usually $HOME/.local/bin, use directly since `binHome` is not defined in `config.xdg` yet),
